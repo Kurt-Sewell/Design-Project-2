2dcf2b446be0a2b7de0695728628d8eab75c6993
astromariia <mariia.shevchenko13@gmail.com>
2025-04-15 11:14:38 -0500

    bluetooth works

---
+++ Bluetooth/bluetooth2.cpp
@@ -0,0 +1,22 @@
+#define HC05_RX 1 
+#define HC05_TX 0
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+
+  Serial.println("HC-05 Bluetooth Test on Teensy");
+}
+
+void loop() {
+  if (Serial1.available()) {
+    char c = Serial1.read();
+    Serial.print(c);
+  }
+
+  if (Serial.available()) {
+    char c = Serial.read();
+    Serial1.print(c);
+  }
+}
